484dc7393f8b29c6f1d875e3231896e1b47c5613
Christian Fleschhut <weltrekordhalter@gmail.com>
2021-02-20 18:12:36 +0100

    Map over records in model to render thumbnails

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,16 +4,27 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.url) ] []
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+        , div [ id "thumbnails" ] (List.map viewThumbnail model)
         ]
 
 
 main =
-    view ""
+    view initialModel
