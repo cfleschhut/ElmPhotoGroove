c0be1b07d38a549cc84dfe120ba1237f9c58a656
Christian Fleschhut <weltrekordhalter@gmail.com>
2021-02-22 01:02:55 +0100

    Render selected thumbnail

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -5,24 +5,34 @@ import Html.Attributes exposing (..)
 
 
 initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
 
 
 urlPrefix =
     "http://elm-in-action.com/"
 
 
-viewThumbnail thumb =
-    img [ src (urlPrefix ++ thumb.url) ] []
+viewThumbnail selectedUrl thumb =
+    img [ src (urlPrefix ++ thumb.url), classList [ ( "selected", selectedUrl == thumb.url ) ] ] []
 
 
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] (List.map viewThumbnail model)
+        , div [ id "thumbnails" ]
+            (List.map
+                (\photo ->
+                    viewThumbnail model.selectedUrl photo
+                )
+                model.photos
+            )
+        , img [ src (urlPrefix ++ "large/" ++ model.selectedUrl), class "large" ] []
         ]
 
 
